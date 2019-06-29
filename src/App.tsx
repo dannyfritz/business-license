b374d76b43cb76140da70ef837c063c6d9d08b8b
Danny Fritz <dannyfritz@gmail.com>
2019-06-28 20:13:06 -0600

    🔧 firebase config, travis config ♻ environment vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { LoadScript, GoogleMap, Marker } from "@react-google-maps/api";
+import ApolloClient, { gql } from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_API_URL
+});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Query
+          query={gql`
+            {
+              activeBusinessLicenses(licenseType: "Short Term Rental") {
+                bfn,
+                entityName,
+                establishmentAddress,
+                latitude,
+                longitude,
+              }
+            }
+          `}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {({ loading, error, data }: any) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
+
+            return (
+              <LoadScript
+                googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API}
+              >
+                <GoogleMap
+                  mapContainerStyle={{ height: "100vh", width: "100vw" }}
+                  zoom={12}
+                  center={{
+                    lat: 39.739346,
+                    lng: -104.991040
+                  }}
+                >
+                  {
+                    data.activeBusinessLicenses.map((abl: any) => (
+                      <Marker
+                        key={abl.bfn}
+                        position={{
+                          lat: abl.latitude,
+                          lng: abl.longitude
+                        }}
+                      />
+                    ))
+                  }
+                </GoogleMap>
+              </LoadScript >
+            )
+          }}
+        </Query>
+      </div>
+    </ApolloProvider>
   );
 }
 
